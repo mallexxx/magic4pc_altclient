bfe51107e134e068b72e097676b27c916444f8db
Nathan Ladwig <netham45@gmail.com>
2023-02-13 17:35:00 -0700

    Update main.go

--- cmd/magic4linux/main.go
+++ cmd/magic4linux/main.go
@@ -10,7 +10,7 @@ import (
 	"os/signal"
 	"syscall"
 
-	"github.com/bendahl/uinput"
+	"github.com/go-vgo/robotgo"
 
 	"github.com/mafredri/magic4linux/m4p"
 )
@@ -21,54 +21,18 @@ const (
 )
 
 func main() {
-	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	ctx,cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer cancel()
-
-	if err := run(ctx); err != nil {
-		panic(err)
-	}
+	run(ctx)
 }
 
 func run(ctx context.Context) error {
-	kbd, err := uinput.CreateKeyboard("/dev/uinput", []byte("magic4linux-keyboard"))
-	if err != nil {
-		return err
-	}
-	defer kbd.Close()
 
-	mouse, err := uinput.CreateMouse("/dev/uinput", []byte("magic4linux-mouse"))
-	if err != nil {
-		return err
-	}
-	defer mouse.Close()
-
-	tp, err := uinput.CreateTouchPad("/dev/uinput", []byte("magic4linux-touchpad"), 0, 1920, 0, 1080)
-	if err != nil {
-		return err
-	}
-	defer tp.Close()
-
-	d, err := m4p.NewDiscoverer(broadcastPort)
-	if err != nil {
-		return err
-	}
-	defer d.Close()
-
-	for {
-		select {
-		case <-ctx.Done():
-			return nil
-
-		case dev := <-d.NextDevice():
-			err = connect(ctx, dev, kbd, mouse, tp)
-			if err != nil {
-				log.Printf("connect: %v", err)
-			}
-		}
-	}
+    dev := m4p.DeviceInfo{IPAddr: "192.168.5.101", Port: 42831}
+	return connect(ctx, dev)
 }
 
-func connect(ctx context.Context, dev m4p.DeviceInfo, kbd uinput.Keyboard, mouse uinput.Mouse, tp uinput.TouchPad) error {
+func connect(ctx context.Context, dev m4p.DeviceInfo) error {
 	addr := fmt.Sprintf("%s:%d", dev.IPAddr, dev.Port)
 	log.Printf("connect: connecting to: %s", addr)
 
@@ -88,60 +52,13 @@ func connect(ctx context.Context, dev m4p.DeviceInfo, kbd uinput.Keyboard, mouse
 		case m4p.InputMessage:
 			log.Printf("connect: got %s: %v", m.Type, m.Input)
 
-			// PoC Kodi keyboard mapping.
 			key := m.Input.Parameters.KeyCode
-			switch key {
-			case m4p.KeyWheelPressed:
-				key = uinput.KeyEnter
-			case m4p.KeyChannelUp:
-				key = uinput.KeyPageup
-			case m4p.KeyChannelDown:
-				key = uinput.KeyPagedown
-			case m4p.KeyLeft:
-				key = uinput.KeyLeft
-			case m4p.KeyUp:
-				key = uinput.KeyUp
-			case m4p.KeyRight:
-				key = uinput.KeyRight
-			case m4p.KeyDown:
-				key = uinput.KeyDown
-			case m4p.Key0:
-				key = uinput.Key0
-			case m4p.Key1:
-				key = uinput.Key1
-			case m4p.Key2:
-				key = uinput.Key1
-			case m4p.Key3:
-				key = uinput.Key1
-			case m4p.Key4:
-				key = uinput.Key1
-			case m4p.Key5:
-				key = uinput.Key1
-			case m4p.Key6:
-				key = uinput.Key1
-			case m4p.Key7:
-				key = uinput.Key1
-			case m4p.Key8:
-				key = uinput.Key1
-			case m4p.Key9:
-				key = uinput.Key1
-			case m4p.KeyRed:
-				key = uinput.KeyStop
-			case m4p.KeyGreen:
-				key = uinput.KeyPlaypause
-			case m4p.KeyYellow:
-				key = uinput.KeyZ
-			case m4p.KeyBlue:
-				key = uinput.KeyC
-			case m4p.KeyBack:
-				key = uinput.KeyBackspace
+			pressed := m.Input.Parameters.IsDown
+			log.Printf("Key: %i ressed: %b", key, pressed)
+			if key == 406 {
+				robotgo.Click("right", pressed)
 			}
-
-			if m.Input.Parameters.IsDown {
-				kbd.KeyDown(key)
-			} else {
-				kbd.KeyUp(key)
-			}
+			
 		case m4p.RemoteUpdateMessage:
 			// log.Printf("connect: got %s: %s", m.Type, hex.EncodeToString(m.RemoteUpdate.Payload))
 
@@ -166,22 +83,23 @@ func connect(ctx context.Context, dev m4p.DeviceInfo, kbd uinput.Keyboard, mouse
 
 			x := coordinate[0]
 			y := coordinate[1]
-			// fmt.Println("Move mouse", x, y)
-			tp.MoveTo(x, y)
-
+			//fmt.Println("Move mouse", x, y)
+			robotgo.Move(int(x),int(y))
+			
 			// log.Printf("connect: %d %d %#v %#v %#v %#v", returnValue, deviceID, coordinate, gyroscope, acceleration, quaternion)
 
 		case m4p.MouseMessage:
+		    l//og.Printf("Type: %s", m.Mouse.Type)
 			switch m.Mouse.Type {
 			case "mousedown":
-				tp.LeftPress()
+			    robotgo.Click("left", true)
 			case "mouseup":
-				tp.LeftRelease()
+			    robotgo.Click("left", false)
 			}
 
 		case m4p.WheelMessage:
-			mouse.Wheel(false, m.Wheel.Delta)
-
+		   // log.Printf("WHEEEEEEEL %i", m.Wheel.Delta)
+			robotgo.Scroll(0,int(m.Wheel.Delta/60))
 		default:
 		}
 	}
